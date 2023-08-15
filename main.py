f0da7f922114c459b0e64aa6cc821fee9a566e0b
JoZerQ <radchenko.egor09@gmail.com>
2023-08-15 19:56:23 +0300

    #

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import logging
-from aiogram import Bot
+from aiogram import Bot, Dispatcher, executor, types
 
+API_TOKEN = "6475496025:AAE4y0J6CgXyN3_Mot9FnKgM48jU3-DJycE"
 
-#test commit
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+    
+if __name__ == "__main__":
+    executor.start_polling(dp,skip_updates=True)
