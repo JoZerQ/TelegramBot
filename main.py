86b162d5c36a25f53a6d457c59a5fa24b3989ec9
JoZerQ <141450378+JoZerQ@users.noreply.github.com>
2023-08-16 15:57:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,38 +14,3 @@ if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
 
 
-# import os
-# import random
-# from aiogram import Bot, Dispatcher, types
-# from aiogram.utils import executor
-# from decouple import config
-
-# bot = Bot(config("API_TOKEN"))
-# dp = Dispatcher(bot)
-
-
-# image_categories = {
-#     'природа': 'nature',
-#     'люди': 'people',
-#     'космос': 'space'
-# }
-
-# image_paths = {
-#     'nature': ['path_to_nature_image1', 'path_to_nature_image2', ...],
-#     'people': ['path_to_people_image1', 'path_to_people_image2', ...],
-#     'space': ['path_to_space_image1', 'path_to_space_image2', ...]
-# }
-
-# @dp.message_handler()
-# async def handle_message(message: types.Message):
-#     for keyword, category in image_categories.items():
-#         if keyword in message.text.lower():
-#             random_image = random.choice(image_paths[category])
-#             with open(random_image, 'rb') as photo:
-#                 await message.reply_photo(photo, caption=f'Random image from {category}')
-#             return
-
-#     await message.reply("Не розумію, що ви хочете. Спробуйте щось інше.")
-
-# if __name__ == '__main__':
-#     executor.start_polling(dp, skip_updates=True)
